700918a588a58ba05a9d2e32f48e472eee7c7c48
Ariel Rodriguez <ariel@volonbolon.net>
2012-05-05 15:09:57 -0300

    Adding Constants and Macros

---
+++ Directions/ConstantsAndMacros.h
@@ -0,0 +1,18 @@
+//
+//  ConstantsAndMacros.h
+//  Directions
+//
+//  Created by Ariel Rodriguez on 5/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Directions_ConstantsAndMacros_h
+#define Directions_ConstantsAndMacros_h
+
+#define BARBUTTON(TITLE, SELECTOR) [[UIBarButtonItem alloc] initWithTitle:TITLE style:UIBarButtonItemStylePlain target:self action:SELECTOR]
+#define BARBUTTONBORDERED(TITLE, SELECTOR) [[UIBarButtonItem alloc] initWithTitle:TITLE style:UIBarButtonItemStyleBordered target:self action:SELECTOR]
+#define IMGBARBUTTON(IMAGE, SELECTOR) [[UIBarButtonItem alloc] initWithImage:IMAGE  style:UIBarButtonItemStylePlain target:self action:SELECTOR]
+#define SYSBARBUTTON(ITEM, SELECTOR) [[UIBarButtonItem alloc] initWithBarButtonSystemItem:ITEM target:self action:SELECTOR]
+#define CUSTOMBARBUTTON(VIEW) [[UIBarButtonItem alloc] initWithCustomView:VIEW]
+
+#endif
