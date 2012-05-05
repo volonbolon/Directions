04c041d7dbedcc7581b069f28dd1bc1918cbc206
Ariel Rodriguez <ariel@volonbolon.net>
2012-05-05 11:06:17 -0300

    Adding view controllers

---
+++ Directions/VBMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBMapViewController.h
+//  Directions
+//
+//  Created by Ariel Rodriguez on 5/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBMapViewController : UIViewController
+
+@end
