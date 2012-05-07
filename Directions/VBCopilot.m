fe0c9ac8ddc5de09df4ded6d7e8a946e7a9a9539
Ariel Rodriguez <ariel@volonbolon.net>
2012-05-07 16:29:16 -0300

    handling notifications

--- Directions/VBCopilot.m
+++ Directions/VBCopilot.m
@@ -7,20 +7,42 @@
 //
 
 #import "VBCopilot.h"
+#import "VBAPIClient.h"
 
 @interface VBCopilot ()
+@property (strong) NSArray *steps; 
 @property (strong, readwrite) NSDictionary *currentStep;
+- (void)handleRouteNotification:(NSNotification *)notification; 
 @end
 
 @implementation VBCopilot
 @synthesize currentStep; 
+@synthesize steps; 
 + (VBCopilot *)sharedCopilot {
     static dispatch_once_t pred;
     static VBCopilot *copilot = nil;
     
     dispatch_once(&pred, ^{ 
         copilot = [[self alloc] init]; 
+        
+        [[NSNotificationCenter defaultCenter] addObserver:copilot
+                                                 selector:@selector(handleRouteNotification:)
+                                                     name:kNewRouteNotificationName 
+                                                   object:nil]; 
+        
+        [[NSNotificationCenter defaultCenter] addObserver:copilot
+                                                 selector:@selector(handleRouteNotification:)
+                                                     name:kRouteFailNotificationName 
+                                                   object:nil];
     });
     return copilot;
 }
+
+- (void)handleRouteNotification:(NSNotification *)notification {
+    NSDictionary *route = [[[[VBAPIClient sharedClient] route] objectForKey:@"routes"] objectAtIndex:0];
+    NSDictionary *leg = [[route objectForKey:@"legs"] objectAtIndex:0]; 
+    [self setSteps:[leg objectForKey:@"steps"]]; 
+                    
+}
+
 @end
